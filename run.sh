c8a6a0c7c29cb2b0fa5a4709b833abb3c4012f19
fengchengli <fengchengli@bytedance.com>
2021-07-20 22:19:46 +0800

    add sth

--- run.sh
+++ run.sh
@@ -4,14 +4,17 @@ pip3 install sentencepiece
 pip3 install sox
 pip3 install typeguard
 pip3 install tensorboardX
-pip3 install torch==1.7.0 torchvision==0.8.1 torchaudio==0.7.1 -f https://download.pytorch.org/whl/cu101/torch_stable.html
-
+pip3 install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 
 # download data
 dbase=/opt/tiger/speech_data
 if [ ! -d $dbase ]; then
     mkdir -p $dbase
 fi
+
+hdfs_dir=hdfs://haruna/byte_speech_sv/home/user/fengchengli/speech_data
+hdfs dfs -get $hdfs_dir/data_aishell.tgz
+hdfs dfs -get $hdfs_dir/data_thchs30.tgz
 
 aishell_url=https://openslr.magicdatatech.com/resources/33
 thchs_url=https://openslr.magicdatatech.com/resources/18
